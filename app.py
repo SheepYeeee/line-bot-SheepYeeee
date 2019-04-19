abcc9bd7f744e89309bce1096d3ffb32828f42b2
SheepYee <34338793+SheepYee@users.noreply.github.com>
2019-04-19 13:55:45 +0800

    show news

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+import requests 
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+import random
 
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('xi3ziO6Yv6J2b4nz1vSLMMIRRTehz9VFkWpgzytaDNpKxhdnRbcGWzORpjZGUJd8cJ4StMvKZ4lYtn9ZYEi80ckyu0hcjdtq9+hFttTk/ztv0uKckGTaOGjbiCuxvY0zDJClw0Hf7Dj1ek11lsb6RgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
 # Channel Secret
-handler = WebhookHandler('a8acf3539e24e1b315f7be19ae0000bf')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -30,11 +34,46 @@ def callback():
         abort(400)
     return 'OK'
 
+#新聞
+def apple_news():
+    target_url = 'https://tw.appledaily.com/new/realtime'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')   
+    content = []
+    for index, data in enumerate(soup.select('div.item a')):
+        if index == 20:           
+            return content
+    
+        title = data.find('img')['alt']
+        link =  data['href']
+        link2 = 'https:'+ data.find('img')['data-src']
+        content.append(title)
+        content.append(link)
+        content.append(link2)
+        print("data：")
+        print(content)   
+    return content
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    msg = event.message.text
+    msg = msg.encode('utf-8')
+    if event.message.text == "你是誰":
+        a="我是SheepYeeee"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=a))
+    elif event.message.text == "蘋果新聞":
+        a=apple_news()
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=a))
+    else: 
+        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token, message)
+    
+
+
 
 import os
 if __name__ == "__main__":
