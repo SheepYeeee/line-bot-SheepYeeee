3c8814c71572d5f95aa60658dcaa1fd6d8148e02
SheepYee <34338793+SheepYee@users.noreply.github.com>
2019-04-19 14:23:05 +0800

    plz work

--- app.py
+++ app.py
@@ -7,32 +7,20 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+
 import requests 
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 import random
+import sys
+import datetime
+
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+line_bot_api = LineBotApi('xi3ziO6Yv6J2b4nz1vSLMMIRRTehz9VFkWpgzytaDNpKxhdnRbcGWzORpjZGUJd8cJ4StMvKZ4lYtn9ZYEi80ckyu0hcjdtq9+hFttTk/ztv0uKckGTaOGjbiCuxvY0zDJClw0Hf7Dj1ek11lsb6RgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('a8acf3539e24e1b315f7be19ae0000bf')
 
 #新聞
 def apple_news():
@@ -57,23 +45,40 @@ def apple_news():
         print(content)   
     return content
 
-# 處理訊息
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route("/", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body, "Signature: " + signature)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+       abort(400)
+
+    return 'OK'
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    msg = msg.encode('utf-8')
-    if event.message.text == "你是誰":
-        a="我是SheepYeeee"
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=a))
-    elif event.message.text == "蘋果新聞":
+    #print(type(msg))
+    msg = msg.encode('utf-8')  
+    if event.message.text == "新聞":
         a=apple_news()
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=a))
-    else: 
-        message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
-    
-
-
+    if event.message.text == "你好":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    elif event.message.text == "貼圖":
+        print("貼圖get")
+        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=1, sticker_id=2))
 
 import os
 if __name__ == "__main__":
