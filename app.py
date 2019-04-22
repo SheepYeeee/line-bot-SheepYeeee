5368b032f4552306fc2f6a20e1dc3617ca14c0f8
SheepYee <34338793+SheepYee@users.noreply.github.com>
2019-04-22 13:46:06 +0800

    2 news web

--- app.py
+++ app.py
@@ -1,86 +1,107 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
+import requests
+import json
+from bs4 import BeautifulSoup
+
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
-
-import requests 
-from bs4 import BeautifulSoup
-from urllib.request import urlretrieve
-import random
-import sys
-import datetime
-
-
-app = Flask(__name__)
-
-
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+app= Flask(__name__)
 line_bot_api = LineBotApi('xi3ziO6Yv6J2b4nz1vSLMMIRRTehz9VFkWpgzytaDNpKxhdnRbcGWzORpjZGUJd8cJ4StMvKZ4lYtn9ZYEi80ckyu0hcjdtq9+hFttTk/ztv0uKckGTaOGjbiCuxvY0zDJClw0Hf7Dj1ek11lsb6RgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a8acf3539e24e1b315f7be19ae0000bf')
 
-#新聞
-def apple_news():
-    target_url = 'https://tw.appledaily.com/new/realtime'
-    print('Start parsing movie ...')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')   
-    content = []
-    for index, data in enumerate(soup.select('div.item a')):
-        if index == 20:           
-            return content
-    
-        title = data.find('img')['alt']
-        link =  data['href']
-        link2 = 'https:'+ data.find('img')['data-src']
-        content.append(title)
-        content.append(link)
-        content.append(link2)
-        print("data：")
-        print(content)   
+@app.route('/')
+def hello():
+    return 'trivago'
+
+#聯合
+def udn_news():
+    url = f'https://udn.com/news/index'
+    resp = requests.get(url)
+    resp.encoding = 'utf-8'
+    soup = BeautifulSoup(resp.text, 'lxml')
+    rowsss = soup.find('div', attrs={'class':'tabs_box_wrapper'})
+    tab=rowsss.find_next('div',attrs={'id':'tab1'})
+    dl=tab.find_next('dl')
+    rows=dl.find_all('dt')
+    content=[]
+
+    for row in rows:
+        title=row.find_next('a')#新聞連結
+        thisurl=title.get('href')
+        content.append(title.text)
+        content.append(thisurl)
+    return content
+#自由時報
+def free_news():
+    url = f'https://news.ltn.com.tw/list/breakingnews/all'
+    resp = requests.get(url)
+    resp.encoding = 'utf-8'
+    soup = BeautifulSoup(resp.text, 'lxml')
+    lists = soup.find('ul', attrs={'class':'list'})
+    rows=lists.find_all('a',attrs={'class':'tit'})
+
+    content=[]
+    i=0
+    for row in rows:
+        time=row.find_next('span')#發布時間
+        titles=time.find_next('p')#新聞標題
+        thisurl=row.get('href')
+        thisurl='https:'+thisurl
+        if i<=9:
+                content.append(time.text)
+                content.append(titles.text)
+                content.append(thisurl)
+        i+=1
     return content
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "Hello World!"
-
-@app.route("/", methods=['POST'])
+@app.route('/callback',methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
-    # handle webhook body
+    app.logger.info("Request body: " + body)
     try:
-        handler.handle(body, signature)
+        handler.handle(body,signature)
     except InvalidSignatureError:
-       abort(400)
-
+        abort(400)
     return 'OK'
-
-
+    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    #print(type(msg))
-    msg = msg.encode('utf-8')  
-    if event.message.text == "新聞":
-        a=apple_news()
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=a))
-    if event.message.text == "你好":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-    elif event.message.text == "貼圖":
-        print("貼圖get")
-        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=1, sticker_id=2))
+    if event.message.text == "聯合":
+        a = udn_news()
+        content='\n'.join(a)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
+    elif event.message.text == "自由":
+        a = free_news()
+        content='\n'.join(a)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
+    elif event.message.text == "你好":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    elif event.message.text == "你是誰":
+        a = "我是SheepYeeee，是個聊天機器人，如果你想看最新新聞，請輸入[聯合]、[自由]，我會為你找出該新聞社最新的新聞"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+    elif event.message.text == "回答我":
+        a = "我盡力了"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+    elif event.message.text == "早安":
+        a = "早安安"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+    elif event.message.text == "找飯店":
+        a = "trivago\n"+"https://tc.trip.com/?Allianceid=742329&SID=1621595&ds_cid=71700000038520126&ds_kid=43700034994699689&utm_source=google&utm_medium=cpc&utm_campaign=GG_SE_TW_zh_Hotel_Competitor_NA_Exact%20TC&gclid=CjwKCAjwqfDlBRBDEiwAigXUaCmd5wlWyxr2oRKU0sJK1cc4WJin_OARz2pd30l0bdsWgUt-DMa7FRoCCMkQAvD_BwE&gclsrc=aw.ds"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+    else:
+        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token,message)
 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=port)
+
